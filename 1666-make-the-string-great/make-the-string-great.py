da4462099fab6152aa9bcb692db6f2ffa63dfca4
Moavia Hassan <moaviahassan112@gmail.com>
2024-12-31 23:37:29 +0500

    Time: 0 ms (100.00%) | Memory: 12.3 MB (17.59%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.py
+++ 1666-make-the-string-great/make-the-string-great.py
@@ -4,44 +4,10 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        
         stack = []
-
         for val in s:
-
             if stack and stack[-1] != val and stack[-1].lower() == val.lower():
                 stack.pop()
             else:
                 stack.append(val)
-
         return "".join(stack)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # stack = []
-        # for val in s:
-        #     if stack and stack[-1] != val and stack[-1].lower() == val.lower():
-        #         stack.pop()
-        #     else:
-        #         stack.append(val)
-        # return "".join(stack)

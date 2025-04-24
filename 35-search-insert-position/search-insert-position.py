145e486bb96fb0dd19c1fc6958ab24a9dc735fc5
Moavia Hassan <moaviahassan112@gmail.com>
2025-04-24 22:26:58 +0500

    Time: 0 ms (100.00%) | Memory: 18.4 MB (73.54%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -1,64 +1,17 @@
-class Solution(object):
-    def searchInsert(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
-        left = 0
-        right = len(nums) - 1
-        while left <= right:
-            mid = (left + right) // 2
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        start = 0
+        end = len(nums) - 1
+        
+        while start <= end:
+
+            mid = (start + end) // 2
+
             if nums[mid] == target:
                 return mid
-            elif target > nums[mid]:
-                left = mid+1
-            elif target < nums[mid]:
-                right = mid-1
-            
-        return left
-
-    
+            if target < nums[mid]:
+                end = mid - 1
+            else:
+                start = mid + 1
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        # left = 0
-        # right = len(nums) - 1
-        # while left <= right:
-        #     mid = (left + right) // 2
-        #     if nums[mid] == target:
-        #         return mid
-        #     elif nums[mid] < target:
-        #         left = mid + 1
-        #     else:
-        #         right = mid - 1
-        
-       
-        # return left
-
-
-        # I have to do this by myself
+        return start

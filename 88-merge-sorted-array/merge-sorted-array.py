05e57e642911d22dc36062763f633affe4a220ef
Moavia Hassan <moaviahassan112@gmail.com>
2025-05-22 20:23:19 +0500

    Time: 0 ms (100.00%) | Memory: 17.7 MB (71.62%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -1,5 +1,8 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
         i = m - 1
         j = n - 1
         k = m + n - 1
@@ -17,5 +20,3 @@ class Solution:
             nums1[k] = nums2[j]
             j -= 1
             k -= 1
-
-        return k
